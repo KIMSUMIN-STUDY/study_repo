7f3d2025ea6a86b5c876d3835b385153d9873c86
KIMSUMIN-STUDY <61537184+KIMSUMIN-STUDY@users.noreply.github.com>
2022-07-16 17:52:47 +0900

    study_week1
    backjooon Coommit

---
+++ baekjoon/week1_2798.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace study
+{
+    class Baekjoon_2798
+    {
+        static int BlackJack(int[] SetData, int[] Data)
+        {
+            int Goal = SetData[1];
+            int min = 0;
+            int sum;
+
+            for (int i = 0; i < Data.Length - 2; i++)
+            {
+                for (int j = i + 1; j < Data.Length - 1; j++)
+                {
+                    for (int k = j + 1; k < Data.Length; k++)
+                    {
+                        sum = Data[i] + Data[j] + Data[k];
+
+                        if (sum == Goal)
+                            return sum;
+
+                        if (sum > Goal)
+                            continue;
+
+                        if (Goal - sum < Goal - min)
+                            min = sum;
+                    }
+                }
+            }
+
+            return min;
+        }
+
+        public static void Main(string[] args)
+        {
+            // 설정값 입력
+            string input = Console.ReadLine();
+
+            string[] temp = input.Split(' ');
+            int[] Settting = new int[temp.Length];
+            int idx = 0;
+
+            foreach (string num in temp)
+            {
+                Settting[idx] = Int32.Parse(num);
+                idx++;
+            }
+
+            // 설정에 맞출 값
+            input = Console.ReadLine();
+
+            temp = input.Split(' ');
+            int[] value = new int[temp.Length];
+            idx = 0;
+
+            foreach (string num in temp)
+            {
+                value[idx] = Int32.Parse(num);
+                idx++;
+            }
+
+            Console.WriteLine(BlackJack(Settting, value));
+        }
+    }
+}
---
+++ baekjoon/week1_2920.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using static System.Console;
+using System.Text;
+using System.IO;
+
+namespace study
+{
+    class Baekjoon_2920
+    {
+        static void Main()
+        {
+            string[] input = ReadLine().Split();
+
+            bool ascending = false;
+            bool descending = false;
+
+            int[] nums = new int[8];
+
+            for (int i = 0; i < 8; i++)
+            {
+                nums[i] = int.Parse(input[i]);
+            }
+
+            int checkValue = nums[0];
+
+            for (int i = 0; i < 8; i++)
+            {
+                if ((nums[0] == 1) && (checkValue <= nums[i]))
+                {
+                    checkValue = nums[i];
+                    ascending = true;
+                }
+                else if ((nums[0] == 8) && (checkValue >= nums[i]))
+                {
+                    checkValue = nums[i];
+                    descending = true;
+                }
+                else
+                {
+                    descending = false;
+                    ascending = false;
+                    break;
+                }
+            }
+
+            if (ascending)
+            {
+                WriteLine("ascending");
+            }
+            else if (descending)
+            {
+                WriteLine("descending");
+            }
+            else
+                WriteLine("mixed");
+        }
+    }
+}
